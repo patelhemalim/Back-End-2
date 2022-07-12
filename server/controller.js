5adaa096ee731bd996519ec5739379b3b43dc90d
Hemali Patel <patelhemalim@gmail.com>
2022-07-12 15:56:18 -0500

    first commit

---
+++ server/controller.js
@@ -0,0 +1,40 @@
+let houses = require('./db.json');
+let globalId=4
+
+module.exports={
+    getHouses:(req,res)=>{
+        res.status(200).send(houses)
+    },
+
+    createHouse:(req,res)=>{
+        const{address,price,imageURL}=req.body;
+        let newHouse={
+            id:globalId,
+            address:address,
+            price: +price,
+            imageURL: imageURL
+        }
+        houses.push(newHouse);
+        globalId++;
+        res.status(200).send(houses)
+    },
+    deleteHouse:(req,res)=>{
+        let index=houses.findIndex(elem=>elem.id===+req.param.id)
+        houses.splice(index,1);
+        res.status(200).send(houses)
+    },
+
+    updateHouse:(req,res)=>{
+        const{type}=req.body;
+        let index=houses.findIndex(elem=>elem.id===+req.params.id)
+        if(type==='minus'&& houses.find[index].price>10000){
+            houses[index].price-=10000;
+            res.status(200).send(houses);
+        }else if (type==='plus'){
+            houses[index].price+=10000;
+            res.status(200).send(houses);
+        }else{
+            res.status(400).send('No more changes')
+        }
+    }
+}
